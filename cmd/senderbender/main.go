d39c900abfadef7dbe77fdbb4c8e47b8b7b28d96
Sergey Shepelev <temotor@gmail.com>
2017-12-02 19:47:41 +0300

    cmd/senderbender was gitignored by mistake

---
+++ cmd/senderbender/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/coreos/go-systemd/daemon"
+	"github.com/fiorix/go-smpp/smpp/pdu"
+	"github.com/fiorix/go-smpp/smpp/pdu/pdufield"
+	"github.com/temoto/senderbender/duraqueue"
+	"github.com/temoto/senderbender/junk"
+	"github.com/temoto/senderbender/smpp_server"
+	"github.com/temoto/senderbender/talk"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	var (
+		flagListenSMPP   = flag.String("listen-smpp", "tcp://:2775", "Listen SMPP on tcp://host:port")
+		flagUpstreamSMPP = flag.String("upstream-smpp", "", "Connect to real SMPP 'tcp://user:pass@host:port?options url2...'")
+		flagState        = flag.String("state", "/tmp/senderbender.sqlite", "Sqlite db url")
+		flagDebug        = flag.Bool("debug", false, "Enable debug logging")
+	)
+	flag.Parse()
+
+	// systemd service
+	sdnotify("READY=0\nSTATUS=init\n")
+	if wdTime, err := daemon.SdWatchdogEnabled(true); err != nil {
+		log.Fatal(err)
+	} else if wdTime != 0 {
+		go func() {
+			for _ = range time.Tick(wdTime) {
+				sdnotify("WATCHDOG=1\n")
+			}
+		}()
+	}
+
+	duraqueue := new(duraqueue.DuraQueue)
+	sender := new(Sender)
+	ctx := context.Background()
+	ctx = junk.ContextSetMap(ctx, map[string]interface{}{
+		"log-debug":            flagDebug,
+		"db-timeout-fast":      3 * time.Second,
+		"db-timeout-max":       11 * time.Second,
+		"db-pull-interval":     37 * time.Second,
+		"queue-size":           13,
+		"duraqueue-pickup-age": 23 * time.Minute,
+		"duraqueue":            duraqueue,
+		"sender":               sender,
+	})
+	duraqueue.Init(ctx, *flagState, TaskHandle)
+	sender.Init(ctx, *flagUpstreamSMPP)
+	smppServer := smpp_server.MustSMPPServer(*flagListenSMPP, nil, SMPPServerHandle)
+	stopAll := func() {
+		duraqueue.Stop()
+		sender.Stop()
+		smppServer.Stop(nil)
+	}
+	waitAll := func() {
+		duraqueue.Wait()
+		sender.Wait()
+		smppServer.Wait()
+	}
+
+	sigShutdownChan := make(chan os.Signal, 1)
+	signal.Notify(sigShutdownChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func(ch <-chan os.Signal, f func()) {
+		<-ch
+		log.Printf("graceful stop")
+		sdnotify("READY=0\nSTATUS=stopping\n")
+		f()
+	}(sigShutdownChan, stopAll)
+
+	sdnotify("READY=1\nSTATUS=work\n")
+	go duraqueue.Run(ctx)
+	go smppServer.Run(ctx)
+
+	waitAll()
+}
+
+func sdnotify(s string) {
+	if _, err := daemon.SdNotify(false, s); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func contextGetDuraQueue(ctx context.Context) *duraqueue.DuraQueue {
+	a := ctx.Value("duraqueue")
+	if a == nil {
+		log.Printf("context[duraqueue] not found")
+		return nil
+	}
+	if b, ok := a.(*duraqueue.DuraQueue); ok {
+		return b
+	}
+	log.Fatalf("context[duraqueue] invalid value %#v", a)
+	return nil
+}
+
+func SMPPServerHandle(ctx context.Context, c *smpp_server.SMPPConn, p pdu.Body) {
+	switch p.Header().ID {
+	case pdu.BindReceiverID:
+		c.Send(c.BuildResp(p))
+		c.Bound = smpp_server.BoundRx
+	case pdu.BindTransmitterID:
+		c.Send(c.BuildResp(p))
+		c.Bound = smpp_server.BoundTx
+	case pdu.BindTransceiverID:
+		c.Send(c.BuildResp(p))
+		c.Bound = smpp_server.BoundRx | smpp_server.BoundTx
+	case pdu.EnquireLinkID:
+		c.Send(c.BuildResp(p))
+	case pdu.UnbindID:
+		c.Bound = smpp_server.Unbound
+	case pdu.SubmitSMID:
+		if !c.IsBoundTx() {
+			log.Printf("smpp_server %s submit_sm without bind_tx ... watevah", c)
+		}
+		msg := bendertalk.MessageFromPdu(p, true)
+		log.Printf("smpp_server %s submit_sm seq=%d from=%s to=%s text='%s'",
+			c, p.Header().Seq, msg.From, msg.To, msg.Text)
+		msg.ScheduleUnix = 1
+		contextGetDuraQueue(ctx).PushTalkSMSMessage(ctx, msg)
+
+		resp := c.BuildResp(p)
+		resp.Header().Status = 0
+		resp.Fields().Set(pdufield.MessageID, "randomshit")
+		c.Send(resp)
+
+		// debug stop after first message
+		time.AfterFunc(1*time.Second, c.StopServer)
+	default:
+		log.Printf("smpp_server %s unknown pdu id %s (%08x) data %#v", c, p.Header().ID, int(p.Header().ID), p)
+	}
+}
+
+func TaskHandle(ctx context.Context, t *duraqueue.Task) {
+	sender := contextGetSender(ctx)
+
+	now := time.Now().UTC().Truncate(time.Second)
+	// t.Scheduled = now.Add(10 * time.Minute)
+	t.Locked = time.Time{}
+	t.Worker = ""
+
+	msg, err := t.ToTalk()
+	if err != nil {
+		log.Printf("BUG TaskHandle Task.ToTalk() t=%s error=%s will leave in queue with schedule=NULL need manual review", t, err)
+		t.Scheduled = time.Time{}
+		t.Store(ctx)
+		return
+	}
+	log.Printf("debug TaskHandle SMSMessage id=%s from=%s to=%s text='%s'", msg.Id, msg.From, msg.To, msg.Text)
+
+	upstream := sender.TxFor(msg)
+	if upstream == nil {
+		log.Printf("TaskHandle no upstream for message=%s will leave in queue with schedule=NULL need manual review", msg)
+		t.Scheduled = time.Time{}
+		t.Store(ctx)
+		return
+	}
+	switch msg.State {
+	case bendertalk.SMPPMessageState_Attempt:
+		response, err := upstream.Submit(msg.ToFiorix())
+		if err != nil {
+			log.Printf("TaskHandle Submit failed msg=%s error=%s will retry", msg, err)
+			t.Scheduled = now.Add(5 * time.Minute)
+			t.Store(ctx)
+			return
+		}
+		msg.Id = response.RespID()
+		msg.State = bendertalk.SMPPMessageState_Accepted
+		t.Scheduled = now.Add(5 * time.Minute)
+		t.Modified = now
+		t.Data, err = msg.Marshal()
+		if err != nil {
+			log.Printf("BUG TaskHandle msg.Marshal() msg=%s error=%s", msg, err)
+		}
+		t.Store(ctx)
+		return
+		// TODO handle long messages, smpp.SubmitLong makes UDH cuts
+	case bendertalk.SMPPMessageState_Accepted:
+		log.Printf("TODO TaskHandle invalid state=%d msg=%s", msg.State, msg)
+	case bendertalk.SMPPMessageState_Enroute:
+		log.Printf("TODO TaskHandle invalid state=%d msg=%s", msg.State, msg)
+	case bendertalk.SMPPMessageState_Expired:
+		log.Printf("TODO TaskHandle invalid state=%d msg=%s", msg.State, msg)
+	case bendertalk.SMPPMessageState_Rejected, bendertalk.SMPPMessageState_Delivered, bendertalk.SMPPMessageState_Deleted,
+		bendertalk.SMPPMessageState_Unknown, bendertalk.SMPPMessageState_Undeliverable, bendertalk.SMPPMessageState_Skipped:
+		t.Archive(ctx)
+		return
+	default:
+		log.Printf("BUG TaskHandle invalid state=%d msg=%s", msg.State, msg)
+	}
+
+	return
+}
---
+++ cmd/senderbender/sender.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"github.com/fiorix/go-smpp/smpp"
+	"github.com/temoto/senderbender/alive"
+	"github.com/temoto/senderbender/talk"
+	"golang.org/x/time/rate"
+	"log"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Sender struct {
+	alive *alive.Alive
+	lk    sync.RWMutex
+	trxs  map[string]*smpp.Transceiver
+}
+
+func queryGetInt(query url.Values, key string) (int, bool) {
+	s := query.Get(key)
+	if s == "" {
+		return 0, false
+	}
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return int(i), true
+}
+
+func (self *Sender) Init(ctx context.Context, s string) {
+	self.lk.Lock()
+	defer self.lk.Unlock()
+	self.alive = alive.NewAlive()
+	self.trxs = make(map[string]*smpp.Transceiver)
+	for _, urlString := range strings.Split(s, " ") {
+		if u, err := url.Parse(urlString); err != nil {
+			log.Printf("url '%s' error %s", urlString, err)
+		} else {
+			trx := &smpp.Transceiver{Addr: u.Host}
+			if u.User != nil {
+				trx.User = u.User.Username()
+				trx.Passwd, _ = u.User.Password()
+			}
+			if r, ok := queryGetInt(u.Query(), "rate_limit"); ok {
+				trx.RateLimiter = rate.NewLimiter(rate.Limit(r), 1 /*burst*/)
+			}
+			trx.Bind()
+			self.trxs[urlString] = trx
+			self.alive.Add(1)
+		}
+	}
+}
+
+func (self *Sender) Stop() {
+	for _, t := range self.trxs {
+		t.Close()
+		self.alive.Done()
+	}
+}
+
+func (self *Sender) Wait() {
+	for _, t := range self.trxs {
+		t.Close()
+	}
+}
+
+func (self *Sender) TxFor(msg *bendertalk.SMSMessage) *smpp.Transceiver {
+	self.lk.RLock()
+	defer self.lk.RUnlock()
+
+	// TODO
+	for _, trx := range self.trxs {
+		// "elegant" way to get random value from map
+		return trx
+	}
+
+	return nil
+}
+
+func contextGetSender(ctx context.Context) *Sender {
+	a := ctx.Value("sender")
+	if a == nil {
+		log.Printf("context[sender] not found")
+		return nil
+	}
+	if b, ok := a.(*Sender); ok {
+		return b
+	}
+	log.Fatalf("context[sender] invalid value %#v", a)
+	return nil
+}
